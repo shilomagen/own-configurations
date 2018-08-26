7aa8d80e326a811a3ea5eb335366156c859e186c
Shilo Mangam <smangam@gmail.com>
2018-08-26 09:14:00 +0300

    first commit

---
+++ aliases.sh
@@ -0,0 +1,8 @@
+alias npmpublic="npm config set registry https://registry.npmjs.org/"
+alias npmprivate="npm config set registry http://npm.dev.wixpress.com"
+alias mysql-start="brew services start mysql"
+alias mysql-stop="brew services stop mysql"
+alias wtf="rm -rf node_modules && rm -f package-lock.json && npm i"
+alias taviuuid="echo f5fbc906-ff49-4d22-ab7e-d5a5960769fb | pbcopy"
+alias yalla="npm start"
+alias ten="npm run "
